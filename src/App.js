7da47db299edc870ab3a6adbc5cc684d4d6fbeb4
DianWeerasekara <dian@hunome.com>
2025-05-15 20:00:15 +0300

    my details initial component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import MyDetails from "./components/myDetails/MyDetails";
 import Topbar from "./components/Topbar/Topbar";
 
 function App() {
   return (
     <>
       <Topbar />
+      <MyDetails />
       <h1>Hello World</h1>
     </>
   );
---
+++ src/components/myDetails/MyDetails.js
@@ -0,0 +1,7 @@
+function MyDetails() {
+    return(
+        <h2>My details container</h2>
+    )
+}
+
+export default MyDetails;
