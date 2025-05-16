370abe1db4bb17d87888aedbddaa3bf5d26972e6
DianWeerasekara <dian@hunome.com>
2025-05-16 19:14:38 +0300

    my details component created

--- src/components/myDetails/MyDetails.js
+++ src/components/myDetails/MyDetails.js
@@ -1,6 +1,38 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './myDetails.css'
+import dmb from '../../images/dmb.jpg'
+
+
 function MyDetails() {
     return(
-        <h2>My details container</h2>
+        <div className='main-div-mydetails'>
+            <Container className='mydetails-main-container'>
+                <Row>
+                    {/* Left Column with image and heading, spans 2+ rows visually */}
+                    <Col md={6} className="image-col">  
+                        <img src={dmb} alt="Profile" className="details-image" />
+                    </Col>
+
+                    {/* Right Column with personal details stacked vertically */}
+                    <Col md={6}>
+                        <Row><Col><h1>Dian Weerasekara</h1></Col></Row>
+                        <Row><Col><h2>Full Stack Developer</h2></Col></Row>
+                        <Row><Col><h4>+358417225598</h4></Col></Row>
+                        <Row><Col><h4>dianweerasekara.dmb@gmail.com</h4></Col></Row>
+                        <Row><Col><h4>Software Engineer | Full Stack Developer | Enthusiastic Learner of New Technologies</h4></Col></Row>
+                        <Row>
+                            <Col>
+                                <a href="/path/to/your-cv.pdf" download className="btn btn-primary mt-3">
+                                    Download CV
+                                </a>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
