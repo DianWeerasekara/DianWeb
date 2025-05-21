a8e4f8d983b663ed5c8c1a38a577b50a053dd25e
DianWeerasekara <dian@hunome.com>
2025-05-21 12:53:21 +0300

    skills component details added

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,7 +1,26 @@
-function Card() {
+import Card from 'react-bootstrap/Card';
+
+function Cards({title, image}) {
     return(
-        <h1>cards</h1>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body className="d-flex align-items-center">
+                {image && (
+                    <img 
+                        src={image} 
+                        alt="Card" 
+                        style={{
+                            width: '40px',
+                            height: '40px',
+                            borderRadius: '50%',
+                            objectFit: 'cover',
+                            marginRight: '10px'
+                        }} 
+                    />
+                )}
+                <Card.Title className="mb-0">{title}</Card.Title>
+            </Card.Body>
+        </Card>
     )
 }
 
-export default Card;
+export default Cards;
